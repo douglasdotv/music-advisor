d9d30c6a5c46faae382199391d091e4330d1b8cf
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-07 17:00:45 -0300

    Include 'read from args feature'

--- src/main/java/br/com/dv/advisor/Main.java
+++ src/main/java/br/com/dv/advisor/Main.java
@@ -1,10 +1,26 @@
 package br.com.dv.advisor;
 
+import br.com.dv.advisor.config.MusicAdvisorConfig;
 import br.com.dv.advisor.controller.MusicAdvisorController;
 
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        Args can be used to change the default values
+        example: -access https://accounts.spotify.com -resource https://api.spotify.com -page 5
+         */
+
+        if (args.length > 1 && args[0].equals("-access")) {
+            MusicAdvisorConfig.SERVER_PATH = args[1];
+        }
+        if (args.length > 2 && args[2].equals("-resource")) {
+            MusicAdvisorConfig.RESOURCE_PATH = args[3] + "/v1/browse";
+        }
+        if (args.length > 3 && args[4].equals("-page")) {
+            MusicAdvisorConfig.ITEMS_PER_PAGE = Integer.parseInt(args[5]);
+        }
+
         MusicAdvisorController.getInstance().run();
     }
 
